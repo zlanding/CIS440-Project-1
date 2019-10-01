c295cae016b3b02e46a9fc5a1fd020b459219df1
tonnesen <gtonnese@asu.edu>
2019-10-01 10:50:20 -0700

    adding jobs functionality pt 1

--- ProjectTemplate/ProjectServices.asmx.cs
+++ ProjectTemplate/ProjectServices.asmx.cs
@@ -135,8 +135,6 @@ namespace ProjectTemplate
             sqlCommand.Parameters.AddWithValue("@passValue", HttpUtility.UrlDecode(pass));
             sqlCommand.Parameters.AddWithValue("@emailValue", HttpUtility.UrlDecode(email));
 
-
-
             //this time, we're not using a data adapter to fill a data table.  We're just
             //opening the connection, telling our command to "executescalar" which says basically
             //execute the query and just hand me back the number the query returns (the ID, remember?).
@@ -158,6 +156,71 @@ namespace ProjectTemplate
             }
             sqlConnection.Close();
             return success;
+        }
+
+        //EXAMPLE OF A SELECT, AND RETURNING "COMPLEX" DATA TYPES
+        [WebMethod(EnableSession = true)]
+        public Jobs[] GetJobs()
+        {
+            //check out the return type.  It's an array of Account objects.  You can look at our custom Account class in this solution to see that it's 
+            //just a container for public class-level variables.  It's a simple container that asp.net will have no trouble converting into json.  When we return
+            //sets of information, it's a good idea to create a custom container class to represent instances (or rows) of that information, and then return an array of those objects.  
+            //Keeps everything simple.
+
+            //WE ONLY SHARE ACCOUNTS WITH LOGGED IN USERS!
+            if (Session["id"] != null)
+            {
+                DataTable sqlDt = new DataTable("jobs");
+
+                string sqlConnectString = getConString();
+                string sqlSelect = "select id, userid, pass, firstname, lastname, email from accounts where active=1 order by lastname";
+
+                MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
+                MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+                //gonna use this to fill a data table
+                MySqlDataAdapter sqlDa = new MySqlDataAdapter(sqlCommand);
+                //filling the data table
+                sqlDa.Fill(sqlDt);
+
+                //loop through each row in the dataset, creating instances
+                //of our container class Account.  Fill each acciount with
+                //data from the rows, then dump them in a list.
+                List<Job> jobs = new List<Job>();
+                for (int i = 0; i < sqlDt.Rows.Count; i++)
+                {
+                    //only share user id and pass info with admins!
+                    if (Convert.ToInt32(Session["admin"]) == 1)
+                    {
+                        jobs.Add(new Job
+                        {
+                            id = Convert.ToInt32(sqlDt.Rows[i]["id"]),
+                            userId = sqlDt.Rows[i]["userid"].ToString(),
+                            password = sqlDt.Rows[i]["pass"].ToString(),
+                            firstName = sqlDt.Rows[i]["firstname"].ToString(),
+                            lastName = sqlDt.Rows[i]["lastname"].ToString(),
+                            email = sqlDt.Rows[i]["email"].ToString()
+                        });
+                    }
+                    else
+                    {
+                        jobs.Add(new Job
+                        {
+                            id = Convert.ToInt32(sqlDt.Rows[i]["id"]),
+                            firstName = sqlDt.Rows[i]["firstname"].ToString(),
+                            lastName = sqlDt.Rows[i]["lastname"].ToString(),
+                            email = sqlDt.Rows[i]["email"].ToString()
+                        });
+                    }
+                }
+                //convert the list of accounts to an array and return!
+                return jobs.ToArray();
+            }
+            else
+            {
+                //if they're not logged in, return an empty array
+                return new Job[0];
+            }
         }
 
 
--- ProjectTemplate/indexActions.js
+++ ProjectTemplate/indexActions.js
@@ -172,4 +172,64 @@ function clearLogon() {
     $('#logonPass').val("");
 }
 
+//this function grabs accounts and loads our account window
+function LoadJobs() {
+    var webMethod = "AccountServices.asmx/GetJobs";
+    $.ajax({
+        type: "POST",
+        url: webMethod,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (msg) {
+            if (msg.d.length > 0) {
+                //let's put our accounts that we get from the
+                //server into our accountsArray variable
+                //so we can use them in other functions as well
+                accountsArray = msg.d;
+                //this clears out the div that will hold our account info
+                $("#jobsBox").empty();
+                //again, we assume we're not an admin unless we see data from the server
+                //that we know only admins can see
+                admin = false;
+                for (var i = 0; i < accountsArray.length; i++) {
+                    //we grab on to a specific html element in jQuery
+                    //by using a  # followed by that element's id.
+                    var acct;
+                    //if they have access to admin-level info (like userid and password) then
+                    //create output that has an edit option
+                    if (accountsArray[i].userId != null) {
+                        acct = "<div class='accountRow' id='acct" + accountsArray[i].id + "'>" +
+                            "<a class='nameTag' href='mailto:" + accountsArray[i].email + "'>" +
+                            accountsArray[i].firstName + " " + accountsArray[i].lastName +
+                            "</a> <a href='#' onclick='LoadAccount(" + accountsArray[i].id + ")' class='optionsTag'>edit</a></div><hr>"
+                        admin = true;
+                    }
+                    //if not, then they're not an admin so don't include the edit option
+                    else {
+                        acct = "<div class='accountRow' id='acct" + accountsArray[i].id + "'>" +
+                            "<a class='nameTag' href='mailto:" + accountsArray[i].email + "'>" +
+                            accountsArray[i].firstName + " " + accountsArray[i].lastName +
+                            "</a></div><hr>"
+                    }
+                    $("#accountsBox").append(
+                        //anything we throw at our panel in the form of text
+                        //will be added to the contents of that panel.  Here
+                        //we're putting together a div that holds info on the
+                        //account as well as an edit link if the user is an admin
+                        acct
+                    );
+                }
+            }
+            //we're showing the account window, so let's track that...
+            accountWindowShowing = true;
+            //...because the ShowMenu function behaves differently depending on
+            //if the account window is showing or not
+            ShowMenu();
+        },
+        error: function (e) {
+            alert("boo...");
+        }
+    });
+}
+
 //END OF YOUR OWN JAVASCRIPt
